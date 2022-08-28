4900a01d34f60380e06091152d443387833f225a
parte-mandar <mandarparte1709@gmail.com>
2022-08-28 12:57:01 +0530

    Callback and setTimeout

--- AsynchronousJS/AsyncJS.js
+++ AsynchronousJS/AsyncJS.js
@@ -5,3 +5,23 @@ console.log("eat");
 console.log("Ice cream");
 console.log("with a");
 console.log("spoon");
+
+// Asynchronous
+
+setTimeout(() => {
+    console.log("and drink in a cup!");
+}, 4000)
+
+console.log("and eat in a plate"); // This is printed before setTimeout()
+
+// Callbacks
+
+function one(step2){
+    console.log("Step1");
+    step2()
+}
+function two(){
+    console.log("Step2");
+}
+
+one(two)
